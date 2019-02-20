dc8e73303e1fa266a6c5a9ae8e9d38ff4c385f9d
willian <willian2005@gmail.com>
2019-02-20 20:30:48 -0300

    Implementando a funcao de calculo de consumo de energia.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SimulationMaster
+Utilizando o repositorio abaixo para o calculo do tempo no ar da mensagem.
+https://github.com/willian2005/lorawan_toa.git
--- energyProve.py
+++ energyProve.py
@@ -3,9 +3,14 @@ sys.path.append("..")
 
 import matplotlib.pyplot as plt
 from lorawan_toa.lorawan_toa import get_toa
+#from lorawan_toa.lorawan_toa import mpsrange
 
 calc_semtech_power_mw = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 calc_semtech_power_dbm = [22, 23, 24, 24, 24, 25, 25, 25, 25, 26, 31, 32, 34, 35, 44, 82, 85, 90, 105, 115, 125]
+
+power_dbm_to_mA = [(0, 22), (7, 25), (14, 44), (20, 125)]
+
+voltage_supply = 3.3
 
 def plotCalcSemtechPower_mA_by_dbm():
     plt.plot(calc_semtech_power_mw, calc_semtech_power_dbm)
@@ -13,28 +18,38 @@ def plotCalcSemtechPower_mA_by_dbm():
     plt.ylabel('power in dBm')
     plt.show()
 
-#def timeInSpectrum(self, dr, bytes, region):
+def energyCalculator(time_on_air, power_in_dbm):
+    '''
+    Parameters
+        time_on_air: time of the packet in ms
+        power: power of transmission in dBm
+
+    Return:
+        Energy used in miliJoule
+    '''
+    power_in_W = -1
+
+    for a,b in power_dbm_to_mA:
+        if a == power_in_dbm:
+            power_in_W = (b/1000)*3.3
+
+    if power_in_W == -1:
+        print ("ERROR: The power: %d dBm is not programmed" % power_in_dbm)
+        exit(-1)
+
+    miliJoule = power_in_W*time_on_air
+
+    return miliJoule
 
 if __name__== "__main__":
-    ret = get_toa(20, 7)
-    print ("PHY payload size    : Bytes",  ret["phy_pl_size"])
+    ret = get_toa(9, 7)
 
-    """
-    print ("MAC payload size    : %d Bytes" % ret["mac_pl_size"])
-    print ("Spreading Factor    : %d" % ret["sf"])
-    print ("Band width          : %d kHz" % ret["bw"])
-    print ("Low data rate opt.  : %s" % ret["ldro"])
-    print ("Explicit header     : %s" % ret["eh"])
-    print ("CR (coding rate)    : %d (4/%d)" % (ret["cr"], 4+ret["cr"]))
-    print ("Symbol Rate         : %.3f symbol/s" % ret["r_sym"])
-    print ("Symbol Time         : %.3f msec/symbol" % ret["t_sym"])
-    print ("Preamble size       : %d symbols" % ret["preamble"])
-    print ("Packet symbol size  : %d symbols" % ret["n_sym_payload"])
-    print ("Preamble ToA        : %.3f msec" % ret["t_preamble"])
-    print ("Payload ToA         : %.3f msec" % ret["t_payload"])
-    print ("Time on Air         : %.3f msec" % ret["t_packet"])
-    print ("Duty Cycle          : %d %%" % ret["duty_cycle"])
-    print ("Min span of a cycle : %.3f sec" % ret["t_cycle"])
-    print ("Max Frames per day  : %d frames" % ret["max_packets_day"])
-    plotCalcSemtechPower_mA_by_dbm()
-"""
+    print ("r_sym: symbol rate in *second* : %d" % ret["r_sym"])
+    print ("t_sym: the time on air in millisecond*. : %d" % ret["t_sym"])
+    print ("t_preamble: : %d" % ret["t_preamble"])
+    print ("v_ceil: : %d" % ret["v_ceil"])
+    print ("symbol_size_payload: : %d" % ret["n_sym_payload"])
+    print ("t_payload: : %d" % ret["t_payload"])
+    print ("t_packet: the time on air in *milisecond*. : %d" % ret["t_packet"])
+
+    print ("Power in miliJoule is: %f" % energyCalculator(ret["t_packet"], 14))
